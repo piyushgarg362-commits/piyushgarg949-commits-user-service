7c56689a5ed70c7069d47a6424b7b6f1c8d239da
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-09-09 22:50:15 -0400

    Create userModel.js

---
+++ src/models/userModel.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@example.com"
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane@example.com"
+  }
+];
+
+module.exports = users;
